5052ea4ce80e68d9cdd84e1fd00fc508d6792dda
ThomasTacoVos <tvvos@hotmail.nl>
2018-09-29 22:44:31 +0200

    Push 1.1 Posts Crud Reactions crud

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\PostPost;
+use App\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $posts = Post::all();
+        return view('Posts.index', compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('posts.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $validated =  $request->validated();
+
+        $post = new Post();
+        $post->title = $request->title;
+        $post->body = $request->body;
+        $post->save();
+
+        return redirect('/posts')->with('status', 'post is added');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        //
+        return view('posts.show', compact('post'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        //
+        return view('posts.edit', compact('post'));
+    }
+
+    /**
+     * @param PostPost $request
+     * @param Post $post
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update(PostPost $request, Post $post)
+    {
+        //
+        $validated = $request->validated();
+
+        $post->title = $request->title;
+        $post->body = $request->body;
+        $post->save();
+
+        return redirect()->action('PostConrtoller@index')->with('correct', 'Post edited');
+    }
+
+    /**
+     * @param Post $post
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Exception
+     */
+    public function destroy(Post $post)
+    {
+        //
+        $post->delete();
+        return redirect()->action('PostController@index')->with('correct', 'Post deleted');
+    }
+}
---
+++ app/Http/Controllers/ReactionController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\PostReaction;
+use App\Reaction;
+use Illuminate\Http\Request;
+
+class ReactionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $reactions = Reaction::all();
+        return view('Reactions.index', compact('reactions'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('reactions.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $validated =  $request->validated();
+
+        $reaction = new Reaction();
+        $reaction->title = $request->title;
+        $reaction->body = $request->body;
+        $reaction->save();
+
+        return redirect('/reaction')->with('status', 'Reaction is added');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Reaction  $reaction
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Reaction $reaction)
+    {
+        //
+        return view('reactions.show', compact('reaction'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Reaction  $reaction
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Reaction $reaction)
+    {
+        //
+        return view('reactions.edit', compact('reaction'));
+    }
+
+    /**
+     * @param PostReaction $request
+     * @param Reaction $reaction
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update(PostReaction $request, Reaction $reaction)
+    {
+        //
+        $validated = $request->validated();
+
+        $reaction->title = $request->title;
+        $reaction->body = $request->body;
+        $reaction->save();
+
+        return redirect()->action('ReactionConrtoller@index')->with('correct', 'Reaction edited');
+    }
+
+    /**
+     * @param Reaction $reaction
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Exception
+     */
+    public function destroy(Reaction $reaction)
+    {
+        //
+        $reaction->delete();
+        return redirect()->action('ReactionController@index')->with('correct', 'Reaction deleted');
+    }
+}
